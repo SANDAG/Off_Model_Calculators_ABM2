7a6432bc6877f083c3fed2b18e55719363993b51
Arash <39972246+arashasadabadi@users.noreply.github.com>
2018-09-19 10:22:56 -0700

    revised R script by Arash

--- off_model_calculators/misc_input/SANDAG_MicrotransitCalculatorTables.R
+++ off_model_calculators/misc_input/SANDAG_MicrotransitCalculatorTables.R
@@ -1,4 +1,5 @@
 # Dora Wu @ WSP USA Inc 07-06-2018
+# edited by Arash Asadabadi @ WSP USA Inc 09-18-2018
 # convert Rosella's SAS script to generate tables for MicroTransit GHG calculator
 # files required to run this script include:
 # 1. ct-ramp output individual trip file
@@ -7,7 +8,6 @@
 # 4. "MicrotransitAreaFlags.csv" file that contains TAZ, MSA, FRED flags, CHARIOT flags for each MGRA
 # This script can be run from command window as: 
 # Rscript.exe --vanilla --verbose SANDAG_MicrotransitCalculatorTables.R > SANDAG_Microtansit.log
-
 # install packages from CRAN if required packages are not installed
 list.of.packages <- c("data.table", "bit64", "tidyverse")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -18,40 +18,51 @@ library(data.table)
 library(bit64)
 library(tidyverse)
 
-# ----------------------------------------------------------------------
-# set working directory, data directory and input file names here
-setwd("G:\\Projects\\SANDAG")  # set working direction, where outputs will be written to
-dataDir     <- "Z:\\transit_related_SANDAG\\"  # where input files are
-tripFile    <- "indivTripData_3.csv"               # ct-ramp output individual trip file 
-flagFile    <- "MicrotransitAreaFlags.csv"         # file contain MAZ and corresponding MSA and FRED area flags
-skimFile    <- "dant_distance.csv"                 # skim file that contains TAZ-to-TAZ distance
-workLocFile <- "..\\wsLocResults_3.csv"            # ct-ramp output work location file
-
-# set analysis year
-YEAR = 2016
-# -----------------------------------------------------------------------
-
-# read in files
-itripClasses <- c(rep("integer", 6), rep("character", 3),
-                  rep("integer", 8))
-itrip <- fread(paste0(dataDir, tripFile), colClasses = itripClasses, nrows = -1)
-
-MTAflags <- read.csv(paste0(dataDir, flagFile), header = T, stringsAsFactors = F)
-MTAflags_colName <- c("mgra", "msa", "TAZ", "fred_flag", "chariot_flag",
-                      "year_fred", "year_chariot")
-colnames(MTAflags) <- MTAflags_colName
-MTAflags <- MTAflags %>%
+Scenario=c("2016","2020","2025nb","2035nb","2035_D","2035_Fx","2035_E_minus_test2")
+for (s in Scenario){
+  # ----------------------------------------------------------------------
+  # set working directory, data directory and input file names here
+  main_dir=paste0("T:/RTP/2019RP/rp19_scen/abm_runs_bod/",s,"/")
+  dataDir     <- paste0(main_dir,"r scripts/")  # where input files are
+  tripFile    <- "output/indivTripData_3.csv"               # ct-ramp output individual trip file 
+  tripFile2    <- "report/indivtrips.csv"               # ct-ramp output individual trip file in report 
+  flagFile    <- "MicrotransitAreaFlags.csv"         # file contain MAZ and corresponding MSA and FRED area flags
+  #skimFile    <- "dant_distance.csv"                 # skim file that contains TAZ-to-TAZ distance
+  workLocFile <- "output/wsLocResults_3.csv"            # ct-ramp output work location file
+  output_dir  <- paste0(dataDir,"output/")
+  # set analysis year
+  #YEAR = 2016
+  # -----------------------------------------------------------------------
+  
+  # read in files
+  itripClasses <- c(rep("integer", 6), rep("character", 3),
+                    rep("integer", 15)) 
+  YEAR= as.numeric(substr(s,1,4))
+  itrip <- fread(paste0(main_dir, tripFile), colClasses = itripClasses, nrows = -1)
+  itrip2 <- fread(paste0(main_dir, tripFile2), nrows = -1)
+  
+  #itrip <- read.csv(paste0(dataDir, tripFile), nrows = 2000)
+  #itrip2 <- read.csv(paste0(dataDir, tripFile2), nrows = 2000)
+  
+  MTAflags <- read.csv(paste0(dataDir, flagFile), header = T, stringsAsFactors = F)
+  MTAflags_colName <- c("mgra", "msa", "TAZ", "fred_flag", "chariot_flag",
+                        "year_fred", "year_chariot")
+  colnames(MTAflags) <- MTAflags_colName
+  MTAflags <- MTAflags %>%
     mutate(fred_flag    = ifelse(year_fred > 2016, 0, fred_flag),
            chariot_flag = ifelse(year_chariot > 2016, 0, chariot_flag))
-
-distance <- fread(paste0(dataDir, skimFile), colClasses = c("integer", "integer", "numeric"),
-                  col.names = c("OTAZ", "DTAZ", "Dist"), nrows = -1)
-workLoc <- fread(paste0(dataDir, workLocFile),
-                 colClasses = c(rep("integer", 12), "numeric", "numeric",
-                                "integer", "numeric", "numeric"), nrows = -1)
-
-# create chariot data summary
-chariot <- workLoc %>% 
+  distance= itrip2%>%
+    select(ORIG_MGRA,DEST_MGRA,TRIP_DIST)%>%
+    rename(orig_mgra=`ORIG_MGRA`,dest_mgra=`DEST_MGRA`,Dist =`TRIP_DIST`)%>%
+    distinct(orig_mgra,dest_mgra, .keep_all = TRUE)
+  #distance <- fread(paste0(dataDir, skimFile), colClasses = c("integer", "integer", "numeric"),
+  #col.names = c("OTAZ", "DTAZ", "Dist"), nrows = -1)
+  workLoc <- fread(paste0(main_dir, workLocFile),
+                   colClasses = c(rep("integer", 12), "numeric", "numeric",
+                                  "integer", "numeric", "numeric"), nrows = -1)
+  
+  # create chariot data summary
+  chariot <- workLoc %>% 
     mutate(home_msa   = MTAflags$msa[match(HomeMGRA, MTAflags$mgra)],
            emp_center = MTAflags$chariot_flag[match(WorkLocation, MTAflags$mgra)]) %>% 
     group_by(home_msa, emp_center) %>% 
@@ -59,36 +70,38 @@ chariot <- workLoc %>%
     ungroup() %>% 
     mutate(year = YEAR) %>% 
     select(year, home_msa, emp_center, workers)
-
-# attach trip distance to trip file, and only count trips 2 miles or shorter
-itrip <- itrip %>% 
-    mutate(OTAZ = MTAflags$TAZ[match(orig_mgra, MTAflags$mgra)],
-           DTAZ = MTAflags$TAZ[match(dest_mgra, MTAflags$mgra)]) 
-           
-itrip <- setDT(itrip)           
-setkey(itrip, OTAZ, DTAZ)     
-setkey(distance, OTAZ, DTAZ)    
-
-itrip <- merge(itrip, distance, all.x = T)
-colnames(MTAflags) <- c(paste0("orig_", MTAflags_colName))
-
-itrip <- itrip %>% 
+  
+  # attach trip distance to trip file, and only count trips 2 miles or shorter
+  #itrip <- itrip %>% 
+  #    mutate(OTAZ = MTAflags$TAZ[match(orig_mgra, MTAflags$mgra)],
+  #           DTAZ = MTAflags$TAZ[match(dest_mgra, MTAflags$mgra)]) 
+  
+  #itrip <- setDT(itrip)           
+  #setkey(itrip, OTAZ, DTAZ)     
+  #setkey(distance, OTAZ, DTAZ)    
+  
+  #itrip <- merge(itrip, distance, all.x = T)
+  colnames(MTAflags) <- c(paste0("orig_", MTAflags_colName))
+  
+  itrip <- itrip %>% 
+    left_join(distance, by = c("orig_mgra"="orig_mgra", "dest_mgra"="dest_mgra"))%>%
     filter(Dist <= 2) %>% 
     left_join(MTAflags, by = "orig_mgra")
-
-colnames(MTAflags) <- c(paste0("dest_", MTAflags_colName))
-
-itrip <- itrip %>% 
+  
+  colnames(MTAflags) <- c(paste0("dest_", MTAflags_colName))
+  
+  
+  itrip <- itrip %>% 
     left_join(MTAflags, by = "dest_mgra")
-
-# create auto trip summary    
-vtrip <- itrip %>% 
+  
+  # create auto trip summary    
+  vtrip <- itrip %>%
     filter(trip_mode <= 8) %>% 
     group_by(orig_msa, orig_fred_flag, dest_msa, dest_fred_flag) %>% 
     summarise(auto_trips = n())
-
-# create person trip summary and merge with auto trip summmary    
-ptrip <- itrip %>% 
+  
+  # create person trip summary and merge with auto trip summmary    
+  ptrip <- itrip %>% 
     group_by(orig_msa, orig_fred_flag, dest_msa, dest_fred_flag) %>% 
     summarise(person_trips = n()) %>% 
     left_join(vtrip, by = c("orig_msa", "orig_fred_flag",
@@ -96,11 +109,10 @@ ptrip <- itrip %>%
     mutate(year = YEAR) %>%
     select(year, orig_msa, orig_fred_flag, dest_msa, dest_fred_flag, 
            person_trips, auto_trips)
-
-# write summary tables out!    
-write.csv(ptrip, "Model Data FRED.csv", row.names = F)
-write.csv(chariot, "Model Data CHARIOT.csv", row.names = F)
-
-
-
+  
+  # write summary tables out!    
+  write.csv(ptrip, paste0(output_dir,"Model Data FRED.csv"), row.names = F)
+  write.csv(chariot, paste0(output_dir,"Model Data CHARIOT.csv"), row.names = F)
+  #####
+}
 
